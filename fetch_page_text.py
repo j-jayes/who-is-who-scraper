f1fea9989e42cc36d7b5bf6086e5a34ecae070c7
Jonathan Jayes (Dell) <j0nathanjayes@gmail.com>
2023-04-21 13:12:53 +0200

    Adding first batch of scraped data

---
+++ fetch_page_text.py
@@ -0,0 +1,44 @@
+import requests
+import time
+from bs4 import BeautifulSoup
+
+def extract_bio_data(page_source):
+    start_tag = "<!-- mode=normal -->"
+    end_tag = "<!-- NEWIMAGE2 -->"
+
+    start_index = page_source.find(start_tag)
+    end_index = page_source.find(end_tag)
+
+    if start_index == -1 or end_index == -1:
+        return None
+
+    start_index += len(start_tag)
+    bio_data = page_source[start_index:end_index].strip()
+
+    return bio_data
+
+# 1061
+
+for i in range(200, 400):
+    url = f"http://runeberg.org/vemarvem/sthlm45/{str(i).zfill(4)}.html"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, "html.parser")
+
+        # Extract the text using the extract_bio_data function
+        bio_data = extract_bio_data(str(soup))
+
+        if bio_data:
+            with open(f"data/raw/page_text_{i}.txt", "w", encoding="UTF-8") as f:
+                f.write(bio_data)
+            print(f"Text has been saved to page_text_{i}.txt")
+
+        else:
+            print(f"Biographical data not found for page {i}.")
+
+        # Wait for a respectful amount of time before making the next request
+        time.sleep(10)
+
+    else:
+        print(f"Failed to fetch page {i}. Status code: {response.status_code}")
