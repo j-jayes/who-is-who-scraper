9164ead923ee0d3ad882087ac08280b2988a720f
Jonathan Jayes (Dell) <j0nathanjayes@gmail.com>
2023-05-14 16:13:55 +0200

    add first bits of translations

--- translate-biographies.py
+++ translate-biographies.py
@@ -1,7 +1,6 @@
 import os
 import openai
 import json
-import glob
 import yaml
 
 with open("config.yaml", "r") as f:
@@ -10,34 +9,37 @@ with open("config.yaml", "r") as f:
 openai.api_key = config["default"]["key"]
 
 def translate_and_structure_text(swedish_text):
-    # Translate the Swedish text to English
-    translate_prompt = f"Translate the following abbreviated Swedish biography to English: {swedish_text}"
-    translation_response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
-        prompt=translate_prompt,
-        temperature=0.7,
-        max_tokens=200,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-    english_text = translation_response.choices[0].text.strip()
+    try:
+        # Translate the Swedish text to English
+        translate_prompt = f"Translate the following abbreviated Swedish biography to English: {swedish_text}"
+        translation_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                {"role": "system", "content": "You are an expert on Swedish family history."},
+                {"role": "user", "content": f"{translate_prompt}"}
+            ]
+        )
 
-    # Structure the biography in Schema.org/Person format
-    structure_prompt = f"Given the original Swedish biography: {swedish_text}\nAnd its English translation: {english_text}\n"\
-                       "Structure the biography in Schema.org/Person format as a JSON object. Ensure the JSON is RFC 8259 compliant."
-    structure_response = openai.Completion.create(
-        engine="gpt-3.5-turbo",
-        prompt=structure_prompt,
-        temperature=0.7,
-        max_tokens=200,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0,
-    )
-    structured_biography = json.loads(structure_response.choices[0].text.strip())
+        english_text = translation_response.choices[0].message.content
 
-    return english_text, structured_biography
+        structure_prompt = f"Given the original Swedish biography: {swedish_text}\nAnd its English translation: {english_text}\n"\
+                           "Structure the biography in Schema.org/Person format as a JSON object. Include dates wherever possible. Only provide a  RFC8259 compliant JSON response."
+
+        structure_response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=[
+                    {"role": "system", "content": "You are an expert on Swedish family history and the Schema.org/Person format."},
+                    {"role": "user", "content": f"{structure_prompt}"}
+            ]
+        )
+
+        structured_biography = json.loads(structure_response.choices[0].message.content)
+
+        return english_text, structured_biography
+
+    except Exception as e:
+        print(f"Error in translate_and_structure_text: {e}")
+        return None, None
 
 
 def main():
@@ -45,29 +47,42 @@ def main():
     output_directory = "data/biographies_translated"
     os.makedirs(output_directory, exist_ok=True)
 
-    # Set the file path to the specified file
-    file_path = os.path.join(input_directory, "gota48_1_biography_1.txt")
+    # Get the list of all .txt files in the input directory
+    all_files = sorted([f for f in os.listdir(input_directory) if f.endswith(".txt")])
 
-    # Read the original Swedish biography
-    with open(file_path, "r", encoding="utf-8") as file:
-        original_biography = file.read()
+    # Process only the first 10 files
+    for file_name in all_files[20:100]:
+        file_path = os.path.join(input_directory, file_name)
 
-    # Translate the biography to English and structure it
-    translated_biography, structured_biography = translate_and_structure_text(original_biography)
+        try:
+            # Read the original Swedish biography
+            with open(file_path, "r", encoding="utf-8") as file:
+                original_biography = file.read()
 
-    # Prepare JSON data
-    data = {
-        "original": original_biography,
-        "translated": translated_biography,
-        "structured": structured_biography,
-    }
+            # Translate the biography to English and structure it
+            translated_biography, structured_biography = translate_and_structure_text(original_biography)
 
-    # Save the JSON data to the output directory
-    output_file_name = os.path.basename(file_path).replace(".txt", ".json")
-    output_file_path = os.path.join(output_directory, output_file_name)
+            if translated_biography is not None and structured_biography is not None:
+                # Prepare JSON data
+                data = {
+                    "original": original_biography,
+                    "translated": translated_biography,
+                    "structured": structured_biography,
+                }
 
-    with open(output_file_path, "w", encoding="utf-8") as output_file:
-        json.dump(data, output_file, ensure_ascii=False, indent=4)
+                # Save the JSON data to the output directory
+                output_file_name = os.path.basename(file_path).replace(".txt", ".json")
+                output_file_path = os.path.join(output_directory, output_file_name)
+
+                with open(output_file_path, "w", encoding="utf-8") as output_file:
+                    json.dump(data, output_file, ensure_ascii=False, indent=4)
+
+                print(f"Processed file: {file_name}")
+            else:
+                print(f"Error processing file {file_name}: translation or structuring failed. Check the API response for more information.")
+
+        except Exception as e:
+            print(f"Error processing file {file_name}: {e}")
 
 if __name__ == "__main__":
     main()
