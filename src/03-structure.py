a5645fb16ca900f0e47e70c7b8b8f1b2efe823e7
Jonathan Jayes <j0nathanjayes@gmail.com>
2023-12-18 16:24:49 +0200

    round 2

--- src/03-structure.py
+++ src/03-structure.py
@@ -222,7 +222,7 @@ def process_json_files(data_dir, file_range):
 
 
 # File range for testing
-FILE_RANGE = [0, 1]
+FILE_RANGE = [0, 5]
 
 # Call the function with the appropriate data directory and file range
 process_json_files(data_dir, FILE_RANGE)
